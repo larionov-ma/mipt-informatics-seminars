7ed2b0628b3273bfbbc0615eee2371f33c0920a3
larionov-ma <larionov.ma@phystech.edu>
2026-09-14 13:42:00 +0300

    finish 2

--- seminar 2/2.py
+++ seminar 2/2.py
@@ -6,12 +6,33 @@ c = int(l/n)
 for i in range(c):
     arr += [t[i*c:i*c+c]]
 
+def get_mirror(arr):
+    mirror = []
+    for i in range(len(arr)-1, -1, -1):
+            mirror.append(arr[i])
+    return mirror
+        
+def array_to_str(arr):
+    result = ''
+    for i in arr:
+        result += i
+    return result
 
 print(arr)
+
 for i in range(len(arr)):
     txt = arr[i]
     txt = [j for j in txt]
     arr[i]=txt
 
-print(arr)
-mirror_arr = []
+
+
+for i in range(len(arr)):
+     arr[i] = get_mirror(arr[i])
+
+
+for i in range(len(arr)):
+     arr[i] = array_to_str(arr[i])
+
+print(array_to_str(arr))
+
